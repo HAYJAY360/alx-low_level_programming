9ac71785dea574ea4648944c68900df6adda191d
Azeez Ajiboye <biollyhanz@gmail.com>
2022-12-10 13:02:50 -0800

    Task 2 - I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	char lowercase;
 
-	for (lowercase = "a"; lowercase <= "z"; lowercase++)
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 		putchar(lowercase);
 
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
