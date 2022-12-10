238a4b4fe290b77627439b1ed2000fa6b7addbf6
Azeez Ajiboye <biollyhanz@gmail.com>
2022-12-10 14:38:58 -0800

    Task 7 - Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a positive, zero or negative number.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a' c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
