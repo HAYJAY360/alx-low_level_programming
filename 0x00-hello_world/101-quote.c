3195e5611a397217a5d8982f96c68e5f463ee5d8
Azeez Ajiboye <biollyhanz@gmail.com>
2022-12-11 13:37:01 -0800

    Task 8 -UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  *
  * Description : A C program that prints without printf function
  *
- * Return: Always 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59");
 	return (1);
 }
