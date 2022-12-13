2a497a823587ac0d0b79039b1427a2def14ce6c4
Azeez Ajiboye <biollyhanz@gmail.com>
2022-12-13 08:40:55 -0800

    Task 10 - a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - This command adds two integers together
+ * @n1: 1st integer
+ * @n2: 2nd integer
+ *
+ * Return: The result
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
