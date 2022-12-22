3a5c7f027ba0d1888c840e6d37ce355649ac6c39
Azeez Ajiboye <biollyhanz@gmail.com>
2022-12-22 06:26:46 -0800

    Task 5 - Always Look Up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,18 @@
  * Return: str
  */
 
-char	*string_toupper(char *str)
+char *string_toupper(char *str)
 {
-	int	i;
+	int i = 0;
 
-	i = 0
+
 	while (str[i])
 	{
-		str[i] = upper(str[i]);
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
 		i++;
 	}
+
 	return (str);
 }
