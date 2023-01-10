4419dee4484684ca73718a744b990d3473947cc2
Azeez Ajiboye <biollyhanz@gmail.com>
2023-01-10 13:29:50 -0800

    Task 2 -  He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - checks the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s;
+
+	s = str_concat("Hello", NULL);
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+
+	s = str_concat(NULL, "Hello");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+
+	s = str_concat(NULL, NULL);
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	s = str_concat("Betty ", "Holberton");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+
+	return (0);
+}
