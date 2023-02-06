9169db9c7c369ab46b46861089b06560dd0b3807
Azeez Ajiboye <biollyhanz@gmail.com>
2023-02-06 13:05:06 -0800

    Task 3 - Copier

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@ void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * @file: The name of the file.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
